f96c4c1f06e3b2520552290b8f96c30b4b2f942d
karambirsingh2520 <karambirsingh2520@gmail.com>
2025-03-21 22:53:27 +0530

    Add newline as valid delimiter along with comma

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,5 @@
 export function add(numbers) {
   if (numbers === "") return 0;
-  const nums = numbers.split(",").map(Number);
+  const nums = numbers.replace(/\n/g, ",").split(",").map(Number);
   return nums.reduce((sum, n) => sum + n, 0);
 }
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -7,3 +7,7 @@ test("returns sum of two numbers", () => {
 test("returns sum of multiple numbers", () => {
   expect(add("1,2,3")).toBe(6);
 });
+
+test("handles newline as delimiter along with comma", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
